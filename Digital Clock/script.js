c144f1cb215fdfca697efad02bb4180a256061af
Francesco Galotto <89752537+subwaybd@users.noreply.github.com>
2022-10-12 20:56:22 +0200

    Update script.js

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -13,6 +13,11 @@ function clockUpdate(){
         h = h - 12;
         ampm = "PM"
     }
+    
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    
     hourEl.innerText = h;
     minuteEl.innerText = m;
     secondsEl.innerText = s;
